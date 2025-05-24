6a32b460cdf247c2baaaf9b2102088346c50781a
SunikhyaNayak <sunikhyanayak451@gmail.com>
2025-05-24 17:28:12 +0530

    Add files via upload

---
+++ python prog5 .py
@@ -0,0 +1,9 @@
+def main():
+    a=int(input("Enter the first number:"))
+    b=int(input("Enter the second number:"))
+    if a>b:
+        print("The bigger number is:, a")
+    else:
+        print("The bigger number is:,b")
+main()
+    
